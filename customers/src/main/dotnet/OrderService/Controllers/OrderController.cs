594874701b0e65f06398c289a90bf689fef26f56
layla-p <layla.porter@gmail.com>
2021-08-24 12:53:49 +0100

    added in dotnet order service

---
+++ customers/src/main/dotnet/OrderService/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OrderService.Models;
+
+namespace OrderService.Controllers
+{
+    [ApiController]
+    [Route("orders")]
+    public class OrderController : Controller
+    {
+        
+        [HttpGet]
+		[Route("{customerId?}")]
+        public List<Order> Get(int customerId)
+        {
+            return RandomOrderGenerator(customerId);
+        }
+
+        private List<Order> RandomOrderGenerator(int customerId)
+        {
+            const int Min = 0;
+            const int Max = 20;
+            var randNum = new Random();
+            
+            int[] idCollection = Enumerable
+                .Repeat(0, randNum.Next(Min, Max))
+                .Select(i => randNum.Next(Min, Max))
+                .ToArray();
+
+            return idCollection.Select(num => new Order { Id = num, CustomerId = customerId }).ToList();
+        }
+        }
+
+    } 
+    
+    
---
+++ customers/src/main/dotnet/OrderService/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OrderService.Models
+{
+    public class Order
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("customerId")]
+        public int CustomerId { get; set; }
+
+    }
+}
