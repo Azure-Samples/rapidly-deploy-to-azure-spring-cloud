c1732284b22e0ed9effeb314f30ddd87c41121c3
layla-p <layla.porter@gmail.com>
2021-08-25 18:55:41 +0100

    dontet app added

---
+++ Orders/Controllers/OrdersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Orders.Controllers
+{
+    [ApiController]
+    [Route("orders")]
+    public class OrdersController : Controller
+    {
+
+		[HttpGet]
+		[Route("{customerId?}")]
+		public List<Order> Get(int customerId)
+		{
+			return RandomOrderGenerator(customerId);
+		}
+
+		private List<Order> RandomOrderGenerator(int customerId)
+		{
+			const int Min = 0;
+			const int Max = 20;
+			var randNum = new Random();
+
+			int[] idCollection = Enumerable
+				.Repeat(0, randNum.Next(Min, Max))
+				.Select(i => randNum.Next(Min, Max))
+				.ToArray();
+
+			return idCollection.Select(num => new Order { Id = num, CustomerId = customerId }).ToList();
+		}
+
+		
+	}
+
+	public class Order
+	{
+		[JsonPropertyName("id")]
+		public int Id { get; set; }
+		[JsonPropertyName("customerId")]
+		public int CustomerId { get; set; }
+	}
+}
---
+++ Orders/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Steeltoe.Common.Hosting;
+
+namespace Orders
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseCloudHosting()
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
