908895284e398eb4f110b64b9500b36afa359edc
Julien Dubois <julien.dubois@gmail.com>
2021-08-26 10:29:46 +0200

    Clean up before the demo

--- customers/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into customer (name)
-values ('Layla');
-insert into customer (name)
-values ('Asir');
-insert into customer (name)
-values ('Julien');
-insert into customer (name)
-values ('Josh');
